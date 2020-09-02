d90e7fa41068919ebbf513a2fcf5496dfcaa6dc6
wakamezake <hotwater1367@gmail.com>
2020-09-02 16:14:32 +0900

    apply black

--- tests/test_mcs_kfold.py
+++ tests/test_mcs_kfold.py
@@ -42,20 +42,15 @@ def test_split(num_cv, shuffle_mc):
 def test_stratified_split(num_cv, shuffle_mc):
     target_cols = ["Survived", "Pclass", "Sex"]
     df = pd.read_csv("tests/test_data/train_titanic.csv")
-    mcskf = MCSKFold(n_splits=num_cv, max_iter=1, shuffle_mc=shuffle_mc,
-                     global_seed=SEED)
+    mcskf = MCSKFold(n_splits=num_cv, max_iter=1, shuffle_mc=shuffle_mc, global_seed=SEED)
     indices = mcskf.split(df=df, target_cols=target_cols)
 
-    tr_ = {col: np.zeros((len(indices), len(df[col].unique())))
-           for col in target_cols}
-    val_ = {col: np.zeros((len(indices), len(df[col].unique())))
-           for col in target_cols}
+    tr_ = {col: np.zeros((len(indices), len(df[col].unique()))) for col in target_cols}
+    val_ = {col: np.zeros((len(indices), len(df[col].unique()))) for col in target_cols}
     for fold, (train_index, valid_index) in enumerate(indices):
         for col in target_cols:
-            tr_[col][fold] = df.iloc[train_index][col].value_counts(
-                normalize=True)
-            val_[col][fold] = df.iloc[valid_index][col].value_counts(
-                normalize=True)
+            tr_[col][fold] = df.iloc[train_index][col].value_counts(normalize=True)
+            val_[col][fold] = df.iloc[valid_index][col].value_counts(normalize=True)
     assert_threshold = 0.1
     for col in target_cols:
         tr_std_per_label = tr_[col].std(axis=0)
